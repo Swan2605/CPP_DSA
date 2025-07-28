c82845ee2f2e1df4cb9acd3517201ab7cbe926a1
Suhani Jaiswal <jaisuhani47@gmail.com>
2025-07-28 21:52:57 +0530

    My first cpp file!

---
+++ helloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"I’m Suhani Jaiswal, a final-year B.Tech student at MIT World Peace University, Pune, with a strong foundation in web development and additional skills in cybersecurity and AI. I work primarily with JavaScript, Python, and front-end frameworks to build responsive and scalable web applications. I have explored areas like malware analysis, cryptography, and AI integration for intelligent systems. My technical skillset includes HTML, CSS, JavaScript, React, Node.js, Python, TensorFlow, and basic tools in cybersecurity.I am continuously learning across domains, and I enjoy building secure, efficient, and user-focused digital solutions."<<endl;
+
+    return 0;
+}
